eedbedec880b4eb5f9645f59db471a0a350ff9a8
smanishkumar <smanishkumar@kaartech.com>
2020-08-11 11:45:27 +0530

    environment fixes

--- portfolioAmal/webApp/src/environments/environment.prod.ts
+++ portfolioAmal/webApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDOvvrQfAe211k-OaPaxk2hjNC-N1j909A',
+    authDomain: 'amal-portfolio.firebaseapp.com',
+    databaseURL: 'https://amal-portfolio.firebaseio.com',
+    projectId: 'amal-portfolio',
+    storageBucket: 'amal-portfolio.appspot.com',
+    messagingSenderId: '463212539145',
+    appId: '1:463212539145:wefb:b945184afcc7007f35b7d8',
+    measurementId: 'G-QY0YT4QQC2',
+  }
 };
